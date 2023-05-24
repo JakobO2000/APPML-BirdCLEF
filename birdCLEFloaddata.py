2a8694f3a049ef8000d6e83f7643b9124f4fb58d
Birk Dissing <zfq417@alumni.ku.dk>
2023-05-24 11:40:55 +0200

    load data prototype 2

--- birdCLEFloaddata.py
+++ birdCLEFloaddata.py
@@ -2,15 +2,18 @@ import pandas
 import torch
 import torchaudio
 import soundfile as sf
-print("Hello world")
-print(torch.__version__)
-print(torchaudio.__version__)
+
 # Takes the directory with the data and loads it
 def load_data(directory):
-    df = pandas.DataFrame(directory+"/train_metadata.csv")
-    audio_ogg = []
-    for file in df['filename']:
-        audio_ogg.append(sf.read(directory+"/"+file))
-    df['audio'] = audio_ogg
+    df = pandas.read_csv(directory+'/train_metadata.csv')
+    # audio_ogg = []
+    #for file in df['filename']:
+    #     audio_ogg.append(sf.read(directory+"/"+file))
+    df['audio'] = audio_ogg[0,:]
+    df['audio'] = audio_ogg[1,:]
+    df['audio'] = audio_ogg[2,:]
     chosen_coloumns = ['latitude', 'longitude', 'common_name', 'rating', 'time', 'audio']
     return df[chosen_coloumns]
+
+df = load_data(r'C:\Users\birk\OneDrive - University of Copenhagen\Documents\KU tid\AppML\APPML-BirdCLEF\data')
+print(df)

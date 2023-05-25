8b2e102c9afa0a7282eadcce77c0fb1b5fbf39c0
Jakob Olsen <Jakob.dam.olsen@gmail.com>
2023-05-25 09:23:13 +0200

    Updated load function v2

--- birdCLEFloaddata.py
+++ birdCLEFloaddata.py
@@ -22,6 +22,23 @@ def load_data(directory):
 
 path = r"C:\Users\zhakk\OneDrive\Skrivebord\Uni\Kandidat\AML-Final\BirdCLEFData"
 
+# Takes the directory with the data and loads it
+def load_audiofile(directory):
+    df = pandas.read_csv(directory+'/train_metadata.csv')
+    audio_ogg = []
+    for file in df['filename']:
+         audio_ogg.append(sf.read(directory+"/train_audio/"+file))
+
+
+    audio_ogg = np.asarray(audio_ogg)
+    df['audio1'] = audio_ogg[:,0]
+    df['audio2'] = audio_ogg[:,1]
+    df['audio3'] = audio_ogg[:,2]
+    chosen_coloumns = ['latitude', 'longitude', 'common_name', 'rating', 'time', 'audio']
+    return df[chosen_coloumns]
+
+path = r"C:\Users\zhakk\OneDrive\Skrivebord\Uni\Kandidat\AML-Final\BirdCLEFData"
+
 df = load_data(path)
 print(df)
 

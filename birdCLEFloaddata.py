950bf0abcd39a2bc7586cbddb56796250e36e33f
JakobO2000 <jakob.dam.olsen@gmail.com>
2023-06-01 16:38:41 +0200

    Created normalization for spectrogram, added new way of selecting data for CNN by finding audio duration first

--- birdCLEFloaddata.py
+++ birdCLEFloaddata.py
@@ -21,7 +21,7 @@ def load_audiofile(filepath):
 
 # Converts ogg audio to waveform and spectrogram. Exact values for melspectrogram function might need to be changed values currently chosen from https://www.kaggle.com/code/awsaf49/birdclef23-pretraining-is-all-you-need-train
 # audio -- Can be filepath from metadata dataframe or numpy array with ogg data
-def get_melspectrogram(audio, sr=32000, n_mels=128, n_fft=2028, hop_length=512, fmax=16000, fmin=20):
+def get_melspectrogram(audio, sr=32000, n_mels=128, n_fft=2028, hop_length=512, fmax=16000, fmin=20,power=2.0,top_db=100):
     if type(audio) is str:
         audio, sr = load_audiofile(audio)
     waveform = torch.from_numpy(audio)
@@ -32,11 +32,14 @@ def get_melspectrogram(audio, sr=32000, n_mels=128, n_fft=2028, hop_length=512, 
                                     hop_length=hop_length, #base value from function in notebook it is calculated as duration_of_audio*sr//(384-1)
                                     f_max=fmax,
                                     f_min=fmin,
+                                    power=2.0
                                     )
     melspectrogram = transform(waveform)
-    multiplier = 10.0 ** (80 / 20.0)
-    db_multiplier = 20.0 / np.log10(multiplier)
-    melspectrogram = torchaudio.functional.amplitude_to_DB(melspectrogram,multiplier=multiplier,amin=1e-10,db_multiplier=db_multiplier)
+
+    melspectrogram = torchaudio.transforms.AmplitudeToDB()(melspectrogram)
+    melspectrogram = torch.nn.functional.normalize(melspectrogram, p=2, dim=0)
+    
+    melspectrogram = (melspectrogram * 255)
 
     return melspectrogram
 
